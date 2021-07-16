5c9eb07840a7c679bf12956ea81ff006f57fdcd3
Mlinzo <vlad.derevyagin89@gmail.com>
2021-07-16 12:29:02 +0200

    Initial Commit

---
+++ KeepSolidApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeepSolidApp
+//
+//  Created by user201878 on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
