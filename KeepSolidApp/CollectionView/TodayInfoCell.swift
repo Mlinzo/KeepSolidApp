6375f33d79791fe6e85596bc5c84c3516434e343
Mlinzo <46653182+Mlinzo@users.noreply.github.com>
2021-08-05 11:31:52 +0300

    Delete KeepSolidApp/CollectionView directory

--- KeepSolidApp/CollectionView/TodayInfoCell.swift
+++
@@ -1,6 +0,0 @@
-//
-//  TodayInfoCell.swift
-//  KeepSolidApp
-//
-//  Created by user201878 on 7/17/21.
-//
--- KeepSolidApp/CollectionView/TodayInfoCollection.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TodayInfoCollection.swift
-//  KeepSolidApp
-//
-//  Created by user201878 on 7/18/21.
-//
-
-import UIKit
-
